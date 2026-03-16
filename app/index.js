aaf496da1941a620f8a8c1c9222b28447d79be64
staxchange <dalen@dwgltd.com>
2026-03-16 15:30:14 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,4 @@
-import AppNavigator from "./navigation/AppNavigator";
+import { registerRootComponent } from "expo";
+import App from "./App";
 
-function startApp() {
-  const app = AppNavigator();
-  console.log("STAX App Started");
-  console.log(Object.keys(app));
-}
-
-startApp();
+registerRootComponent(App);
