18b0b452a71077aaf68700f792f9a2eb5b21315a
staxchange <dalen@dwgltd.com>
2026-03-19 17:59:43 -0600

    Update SectionCard.tsx

--- stax/components/SectionCard.tsx
+++ stax/components/SectionCard.tsx
@@ -1,6 +1,12 @@
 import { View, Text } from "react-native";
 
-export default function SectionCard({ title, subtitle }: { title: string; subtitle: string }) {
+export default function SectionCard({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle: string;
+}) {
   return (
     <View
       style={{
